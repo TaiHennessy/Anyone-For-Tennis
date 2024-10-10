1745f20cd98ab8918c6b247c61cb522274922a92
Tai Hennessy <85157848+TaiHennessy@users.noreply.github.com>
2024-10-10 17:45:06 +0930

    joyato

--- AnyoneForTennis/Migrations/LocalDb/20241009114154_eightary4.Designer.cs
+++ AnyoneForTennis/Migrations/LocalDb/20241009114154_eightary4.Designer.cs
@@ -4,6 +4,7 @@ using AnyoneForTennis.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AnyoneForTennis.Migrations.LocalDb
 {
     [DbContext(typeof(LocalDbContext))]
-    partial class LocalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241009114154_eightary4")]
+    partial class eightary4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AnyoneForTennis/Models/SeedData.cs
+++ AnyoneForTennis/Models/SeedData.cs
@@ -3,83 +3,170 @@ using Microsoft.Extensions.DependencyInjection;
 using AnyoneForTennis.Data;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
-namespace AnyoneForTennis.Models;
-
-public static class SeedData
+namespace AnyoneForTennis.Models
 {
-    public static async void Initialize(IServiceProvider serviceProvider)
+    public static class SeedData
     {
-        using (var context = new LocalDbContext(serviceProvider.GetRequiredService<DbContextOptions<LocalDbContext>>()))
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
         {
-            // Identify any Users
-            if (!context.Users.Any())
+            using (var coachContext = new Hitdb1Context(serviceProvider.GetRequiredService<DbContextOptions<Hitdb1Context>>()))
+            using (var localContext = new LocalDbContext(serviceProvider.GetRequiredService<DbContextOptions<LocalDbContext>>()))
             {
-                context.Users.AddRange(
-                new User
-                {
-                    Username = "Big Boy",
-                    Password = "safepassword",
-                    IsAdmin = true
-                },
+                Console.WriteLine("SeedData: Initializing data...");
 
-                new User
-                {
-                    Username = "John Smith",
-                    Password = "123456",
-                    IsAdmin = false
-                },
+                // Ensure local database is created
+                await localContext.Database.EnsureCreatedAsync();
 
-                new User
-                {
-                    Username = "No Name",
-                    Password = "No Password",
-                    IsAdmin = false
-                },
+                // Fetch coaches from dbo.Coaches in Hitdb1Context (source database)
+                var coachesFromSource = await coachContext.Coaches
+                    .FromSqlRaw("SELECT CoachId, FirstName, LastName, Biography, Photo FROM dbo.Coaches")
+                    .ToListAsync();
 
-                new User
+                Console.WriteLine($"SeedData: Number of coaches fetched from source: {coachesFromSource.Count}");
+
+                // Fetch coaches from dbo.Coach in LocalDbContext (local database)
+                var localCoaches = await localContext.Coach.ToListAsync();
+                Console.WriteLine($"SeedData: Number of coaches in local database: {localCoaches.Count}");
+
+                // Begin transaction
+                using (var transaction = await localContext.Database.BeginTransactionAsync())
                 {
-                    Username = "Luigi Mortadella",
-                    Password = "parmesan",
-                    IsAdmin = false
+                    try
+                    {
+                        // Enable IDENTITY_INSERT for dbo.Coach
+                        await localContext.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT dbo.Coach ON;");
+
+                        // Update or Add coaches based on source data
+                        foreach (var sourceCoach in coachesFromSource)
+                        {
+                            var existingCoach = localCoaches.FirstOrDefault(c => c.CoachId == sourceCoach.CoachId);
+
+                            if (existingCoach != null)
+                            {
+                                // Update the coach if details have changed
+                                if (existingCoach.FirstName != sourceCoach.FirstName ||
+                                    existingCoach.LastName != sourceCoach.LastName ||
+                                    existingCoach.Biography != sourceCoach.Biography ||
+                                    existingCoach.Photo != sourceCoach.Photo)
+                                {
+                                    existingCoach.FirstName = sourceCoach.FirstName;
+                                    existingCoach.LastName = sourceCoach.LastName;
+                                    existingCoach.Biography = sourceCoach.Biography ?? "Default biography";
+                                    existingCoach.Photo = sourceCoach.Photo;
+
+                                    Console.WriteLine($"SeedData: Updated coach: {sourceCoach.FirstName} {sourceCoach.LastName}");
+                                }
+                            }
+                            else
+                            {
+                                // Add the new coach
+                                localContext.Coach.Add(new Coach
+                                {
+                                    CoachId = sourceCoach.CoachId,
+                                    FirstName = sourceCoach.FirstName,
+                                    LastName = sourceCoach.LastName,
+                                    Biography = sourceCoach.Biography ?? "Default biography",
+                                    Photo = sourceCoach.Photo
+                                });
+
+                                Console.WriteLine($"SeedData: Added new coach: {sourceCoach.FirstName} {sourceCoach.LastName}");
+                            }
+                        }
+
+                        // Find and remove coaches that exist in the local database but are no longer in the source database
+                        foreach (var localCoach in localCoaches)
+                        {
+                            if (!coachesFromSource.Any(c => c.CoachId == localCoach.CoachId))
+                            {
+                                localContext.Coach.Remove(localCoach);
+                                Console.WriteLine($"SeedData: Removed coach: {localCoach.FirstName} {localCoach.LastName}");
+                            }
+                        }
+
+                        // Save changes
+                        await localContext.SaveChangesAsync();
+
+                        // Disable IDENTITY_INSERT for dbo.Coach
+                        await localContext.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT dbo.Coach OFF;");
+
+                        // Commit transaction
+                        await transaction.CommitAsync();
+                        Console.WriteLine("SeedData: Data synchronized successfully with dbo.Coach.");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Rollback transaction if there's an error
+                        await transaction.RollbackAsync();
+                        Console.WriteLine("SeedData: Error occurred: " + ex.Message);
+                    }
                 }
-                );
-            }
-            
 
-            // Seeding Schedule Data - Looks for any schedules
-            if (!context.Schedule.Any())
-            {
-                context.Schedule.AddRange(
-                new Schedule
+                // Seed Users if none exist in LocalDbContext
+                if (!await localContext.Users.AnyAsync())
                 {
-                    Name = "Super Tennis Training",
-                    Location = "Court D",
-                    Description = "Training for Winners"
-                },
-                new Schedule
-                {
-                    Name = "Defensive Tennis Drills",
-                    Location = "Court A",
-                    Description = "Defense is the best Offence"
-                },
-                new Schedule
-                {
-                    Name = "Tennis for Beginners",
-                    Location = "Court C",
-                    Description = "Training for Beginners"
-                },
-                new Schedule
-                {
-                    Name = "Ultra Marathon Tennis",
-                    Location = "Court B",
-                    Description = "Not for the weak willed"
+                    localContext.Users.AddRange(
+                        new User
+                        {
+                            Username = "Big Boy",
+                            Password = "safepassword", // Ideally hash the password here for security
+                            IsAdmin = true
+                        },
+                        new User
+                        {
+                            Username = "John Smith",
+                            Password = "123456", // Ideally hash the password here for security
+                            IsAdmin = false
+                        },
+                        new User
+                        {
+                            Username = "No Name",
+                            Password = "No Password", // Ideally hash the password here for security
+                            IsAdmin = false
+                        },
+                        new User
+                        {
+                            Username = "Luigi Mortadella",
+                            Password = "parmesan", // Ideally hash the password here for security
+                            IsAdmin = false
+                        }
+                    );
+                    await localContext.SaveChangesAsync(); // Save changes after adding users
                 }
-            );
+
+                // Seed Schedule Data if none exist in LocalDbContext
+                if (!await localContext.Schedule.AnyAsync())
+                {
+                    localContext.Schedule.AddRange(
+                        new Schedule
+                        {
+                            Name = "Super Tennis Training",
+                            Location = "Court D",
+                            Description = "Training for Winners"
+                        },
+                        new Schedule
+                        {
+                            Name = "Defensive Tennis Drills",
+                            Location = "Court A",
+                            Description = "Defense is the best Offence"
+                        },
+                        new Schedule
+                        {
+                            Name = "Tennis for Beginners",
+                            Location = "Court C",
+                            Description = "Training for Beginners"
+                        },
+                        new Schedule
+                        {
+                            Name = "Ultra Marathon Tennis",
+                            Location = "Court B",
+                            Description = "Not for the weak willed"
+                        }
+                    );
+                    await localContext.SaveChangesAsync(); // Save changes after adding schedules
+                }
             }
-            await context.SaveChangesAsync();
         }
     }
 }
-
-
